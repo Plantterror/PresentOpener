08358589b555b5bea5fbbb24d77e849b648d7abc
Planterror <47764567+Plantterror@users.noreply.github.com>
2020-02-05 15:32:09 -0700

    I'm stupid

--- PresentOpener.cs
+++ PresentOpener.cs
@@ -11,33 +11,16 @@ namespace PresentOpener
 		public static string GithubUserName => "Plantterror"; //These 2 lines are for ModHelpers Github intergration.
 		public static string GithubProjectName => "PresentOpener";
 		internal static PresentOpenerConfig Config;
-		internal static RecipeBuilder Recipes;
 		internal static PresentOpener Instance;
-		public override void Unload() //Unloading ModRecipes and ModConfig
+		public override void Unload() //Unloading ModConfig and Instance
 		{
-			Recipes = null;
 			Config = null;
 			Instance = null;
 		}
-		public override void Load() //Loading ModRecipes and ModConfig
+		public override void Load() //Loading ModConfig and making an instance
 		{
 			Config = new PresentOpenerConfig();
-			Recipes = new RecipeBuilder();
 			Instance = new PresentOpener();
-		}
-		public override void AddRecipeGroups() //Recipe groups for Ice Queen and Pumpking weapon drops, respectively. Also Added an Evil Hardmode Crafting Ingredient group.
-		{
-			RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Evil Hardmode Material", new int[]
-			{
-					ItemID.CursedFlame,
-					ItemID.Ichor
-			});
-			RecipeGroup.RegisterGroup("EvilHardmode", group);
-		}
-
-		public override void AddRecipes() //Recipes are moved to the RecipeBuilder, since having all 75+ recipes here would be chaos.
-		{
-			Recipes.LoadRecipes(this);
 		}
 	}
 }
